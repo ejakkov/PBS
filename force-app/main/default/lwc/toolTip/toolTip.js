cf1b87e5a660f73a34511741eec5feaafbcbed96
ejakkov <ervins203@gmail.com>
2024-02-29 19:21:06 +0200

    tooltip update

--- force-app/main/default/lwc/toolTip/toolTip.js
+++ force-app/main/default/lwc/toolTip/toolTip.js
@@ -1,6 +1,11 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
 export default class ToolTip extends LightningElement {
+    @api iconType = 'utility:info'; // Default icon type
+
+    renderedCallback(){
+        console.log(this.iconType);
+    }
     handleMouseEnter() {
         this.template.querySelector('.tooltip').classList.add('show');
     }
