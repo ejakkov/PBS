a976427596ea557982149f9535d5d29445fd344c
ejakkov <ervins203@gmail.com>
2024-03-05 09:38:41 +0200

    cmp library update

--- force-app/main/default/lwc/toolTipDescription/toolTipDescription.js
+++ force-app/main/default/lwc/toolTipDescription/toolTipDescription.js
@@ -1,3 +1,5 @@
 import { LightningElement } from 'lwc';
 
-export default class ToolTipDescription extends LightningElement {}
+export default class ToolTipDescription extends LightningElement {
+    
+}
