9d671ee11344060e0469daceaf7c57253d63fdac
ejakkov <ervins203@gmail.com>
2024-03-06 07:47:09 +0200

    library navigation

--- force-app/main/default/lwc/componentLibrary/componentLibrary.js
+++ force-app/main/default/lwc/componentLibrary/componentLibrary.js
@@ -3,13 +3,41 @@ import cssStyle from '@salesforce/resourceUrl/PBS_CSS';
 import { loadStyle } from 'lightning/platformResourceLoader';
 
 export default class ComponentLibrary extends LightningElement {
-    @track componentNames = [];
-    apiAttributes = [
-        { key: '1', apiName: 'apiName1', defaultValue: 'defaultValue1', description: 'description1' },
-        { key: '2', apiName: 'apiName2', defaultValue: 'defaultValue2', description: 'description2' },
-        // Add more API attributes as needed
-    ];
     renderedCallback() {
         loadStyle(this, cssStyle);
+        // Add event listeners to tab links
+        const tabLinks = this.template.querySelectorAll('.slds-vertical-tabs__link');
+        tabLinks.forEach(link => {
+            link.addEventListener('click', this.handleTabClick.bind(this));
+        });
+    }
+
+    handleTabClick(event) {
+        event.preventDefault();
+        console.log('clicked');
+        const clickedTab = event.target.closest('.slds-vertical-tabs__nav-item');
+        const tabContents = this.template.querySelectorAll('.slds-vertical-tabs__content');
+
+        // Remove active state from all tabs
+        this.template.querySelectorAll('.slds-vertical-tabs__nav-item').forEach(tab => {
+            tab.classList.remove('slds-has-focus');
+            tab.querySelector('.slds-vertical-tabs__link').setAttribute('aria-selected', 'false');
+        });
+
+        // Add active state to the clicked tab
+        clickedTab.classList.add('slds-has-focus');
+        clickedTab.querySelector('.slds-vertical-tabs__link').setAttribute('aria-selected', 'true');
+
+        // Hide all tab contents
+        tabContents.forEach(content => {
+            content.classList.remove('slds-show');
+            content.classList.add('slds-hide');
+        });
+
+        // Show the corresponding tab content
+        const tabId = clickedTab.querySelector('.slds-vertical-tabs__link').getAttribute('aria-controls');
+        const activeContent = this.template.querySelector(`#${tabId}`);
+        activeContent.classList.remove('slds-hide');
+        activeContent.classList.add('slds-show');
     }
 }
