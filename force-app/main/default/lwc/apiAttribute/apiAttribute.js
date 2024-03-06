6b2ceaddca0c9b51e09efb3a8739d77bc797be05
ejakkov <ervins203@gmail.com>
2024-03-06 07:29:14 +0200

    cmp update

--- force-app/main/default/lwc/apiAttribute/apiAttribute.js
+++
@@ -1,7 +0,0 @@
-import { LightningElement, api } from 'lwc';
-
-export default class ApiAttribute extends LightningElement {
-    @api apiName;
-    @api defaultValue;
-    @api description;
-}
--- force-app/main/default/lwc/toolTipDescription/toolTipDescription.js
+++ force-app/main/default/lwc/toolTipDescription/toolTipDescription.js
@@ -1,5 +1,7 @@
 import { LightningElement } from 'lwc';
 
 export default class ToolTipDescription extends LightningElement {
-    
+    myVal = '<pre><code class="language-html">&lt;div class="container"&gt;\n    &lt;p class="slds-text-heading_large"&gt;Address&lt;/p&gt;\n    &lt;p class="slds-text-heading_medium"&gt;Current address:&lt;/p&gt;\n    &lt;c-tool-tip&gt;\n        &lt;span slot="tooltipText"&gt;Please confirm your client\'s current address&lt;/span&gt;\n    &lt;/c-tool-tip&gt;\n&lt;/div&gt;</code></pre>';
+
+
 }
