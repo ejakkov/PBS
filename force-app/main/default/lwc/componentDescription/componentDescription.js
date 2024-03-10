e5c8460b73b57e4243622ce99c77c3a680a22473
ejakkov <ervins203@gmail.com>
2024-03-10 14:35:37 +0200

    cmp update

--- force-app/main/default/lwc/componentDescription/componentDescription.js
+++ force-app/main/default/lwc/componentDescription/componentDescription.js
@@ -5,14 +5,4 @@ export default class ComponentDescription extends LightningElement {
     @api apiName;
     @api defaultValue;
     @api description;
-    apiAttributes = [];
-    connectedCallback() {
-        // Populate apiAttributes array with the attributes received from the parent component
-        this.apiAttributes = this.attributes.map((attr, index) => ({
-            key: index.toString(),
-            apiName: attr.apiName || '',
-            defaultValue: attr.defaultValue || '',
-            description: attr.description || ''
-        }));
-    }
 }
