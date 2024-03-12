2c196708d609988ff9882b61a6bdbaac4082a179
ejakkov <ervins203@gmail.com>
2024-03-12 08:55:10 +0200

    cmp update

--- force-app/main/default/lwc/cardBaseParent/cardBaseParent.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement } from 'lwc';
-import cssStyle from '@salesforce/resourceUrl/PBS_CSS';
-import { loadStyle } from 'lightning/platformResourceLoader';
-
-export default class CardBaseParent extends LightningElement {
-    renderedCallback() {
-        loadStyle(this, cssStyle);
-    }
-}
