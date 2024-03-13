6db6e8de3769896dcf982a8f6f4b68700efb9d7a
ejakkov <ervins203@gmail.com>
2024-03-13 10:38:15 +0200

    cmp update

--- force-app/main/default/lwc/componentLibrary/componentLibrary.js
+++ force-app/main/default/lwc/componentLibrary/componentLibrary.js
@@ -3,6 +3,22 @@ import cssStyle from '@salesforce/resourceUrl/PBS_CSS';
 import { loadStyle } from 'lightning/platformResourceLoader';
 
 export default class ComponentLibrary extends LightningElement {
+ 
+    tabs = [
+        {
+            id: "slds-vertical-tabs-0__nav",
+            ariaControls: "slds-vertical-tabs-0",
+            dataTabId: "0",
+            title: "Tool Tip"
+        },
+        {
+            id: "slds-vertical-tabs-1__nav",
+            ariaControls: "slds-vertical-tabs-1",
+            dataTabId: "1",
+            title: "Card Base"
+        }
+    ];
+
     renderedCallback() {
         loadStyle(this, cssStyle);
         // Add event listeners to tab links
@@ -18,23 +34,17 @@ export default class ComponentLibrary extends LightningElement {
         const clickedTab = event.target.closest('.slds-vertical-tabs__nav-item');
         const tabContents = this.template.querySelectorAll('.slds-vertical-tabs__content');
 
-        // Remove active state from all tabs
         this.template.querySelectorAll('.slds-vertical-tabs__nav-item').forEach(tab => {
             tab.classList.remove('slds-has-focus');
-            tab.querySelector('.slds-vertical-tabs__link').setAttribute('aria-selected', 'false');
         });
 
-        // Add active state to the clicked tab
         clickedTab.classList.add('slds-has-focus');
-        clickedTab.querySelector('.slds-vertical-tabs__link').setAttribute('aria-selected', 'true');
 
-        // Hide all tab contents
         tabContents.forEach(content => {
             content.classList.remove('slds-show');
             content.classList.add('slds-hide');
         });
 
-        // Show the corresponding tab content
         const tabId = clickedTab.querySelector('.slds-vertical-tabs__link').getAttribute('aria-controls');
         const activeContent = this.template.querySelector(`#${tabId}`);
         activeContent.classList.remove('slds-hide');
