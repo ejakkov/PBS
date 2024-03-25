6963da8c71bab92051095e401c2e5512677ac7f9
ejakkov <ervins203@gmail.com>
2024-03-25 11:26:14 +0200

    cmp update

--- force-app/main/default/lwc/baseModal/baseModal.js
+++ force-app/main/default/lwc/baseModal/baseModal.js
@@ -2,17 +2,12 @@ import { LightningElement, api } from 'lwc';
 
 export default class BaseModal extends LightningElement {
     @api isOpen = false;
-    connectedCallback() {
-        console.log(this.isOpen);
-    }
 
-    openModal() {
-        this.isOpen = true;
-    }
+    @api open() {
+    this.isOpen = true;
+  }
 
     closeModal() {
-        console.log('close');
         this.isOpen = false;
-        console.log(this.isOpen);
     }
 }
--- force-app/main/default/lwc/baseModalDescription/baseModalDescription.js
+++ force-app/main/default/lwc/baseModalDescription/baseModalDescription.js
@@ -2,8 +2,37 @@ import { LightningElement, track } from 'lwc';
 
 export default class BaseModalDescription extends LightningElement {
     @track isOpen = false;
+    apiAttributes = [
+        {
+            id: '1',
+            apiName: 'variant',
+            defaultValue: 'A',
+            description: 'Sets the card base variant'
+        },
+        {
+            id: '2',
+            apiName: 'is-separator-visible',
+            defaultValue: 'false',
+            description: 'Sets a separator line between header and body'
+        },
+        {
+            id: '3',
+            apiName: 'is-footer-centered',
+            defaultValue: 'false',
+            description: 'Makes the footer centered'
+        }
+    ];
     handleClick(){
         this.isOpen = true;
         console.log(this.isOpen);
     }
+    handleClickClose(){
+        this.isOpen = false;
+        console.log(this.isOpen);
+    }
+
+    handleOpenModal() { 
+        const modal = this.template.querySelector("c-base-modal");
+        modal.open();
+      }
 }
