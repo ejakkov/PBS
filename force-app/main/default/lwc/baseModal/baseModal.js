5a85dd18f5dd29bd8748f908d86617d56d4e24f5
ejakkov <ervins203@gmail.com>
2024-03-26 14:25:18 +0200

    cmp update

--- force-app/main/default/lwc/baseModal/baseModal.js
+++ force-app/main/default/lwc/baseModal/baseModal.js
@@ -2,7 +2,20 @@ import { LightningElement, api } from 'lwc';
 
 export default class BaseModal extends LightningElement {
     @api isOpen = false;
+    modalWidth;
 
+    renderedCallback() {
+      if (this.isOpen) {
+        const contentElement = this.template.querySelector('.slds-modal__content');
+        if (contentElement) {
+          const contentWidth = contentElement.offsetWidth;
+          const maxWidth = 600; 
+          if (maxWidth < contentWidth){}
+          this.modalWidth = `min-width: ${Math.min(contentWidth + 40, maxWidth)}px;`;
+        }
+      }
+    }
+    
     @api open() {
     this.isOpen = true;
   }
@@ -10,4 +23,5 @@ export default class BaseModal extends LightningElement {
     closeModal() {
         this.isOpen = false;
     }
+
 }
