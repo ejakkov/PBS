605584056d43727f0f0e07167e6716dfcb7cbbf6
ejakkov <ervins203@gmail.com>
2024-03-26 17:34:15 +0200

    cmp update

--- force-app/main/default/lwc/baseModal/baseModal.js
+++ force-app/main/default/lwc/baseModal/baseModal.js
@@ -1,20 +1,7 @@
 import { LightningElement, api } from 'lwc';
 
 export default class BaseModal extends LightningElement {
-    @api isOpen = false;
-    modalWidth;
-
-    renderedCallback() {
-      if (this.isOpen) {
-        const contentElement = this.template.querySelector('.slds-modal__content');
-        if (contentElement) {
-          const contentWidth = contentElement.offsetWidth;
-          const maxWidth = 600; 
-          if (maxWidth < contentWidth){}
-          this.modalWidth = `min-width: ${Math.min(contentWidth + 40, maxWidth)}px;`;
-        }
-      }
-    }
+    isOpen = false;
     
     @api open() {
     this.isOpen = true;
--- force-app/main/default/lwc/baseModalDescription/baseModalDescription.js
+++ force-app/main/default/lwc/baseModalDescription/baseModalDescription.js
@@ -1,35 +1,23 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement} from 'lwc';
 
 export default class BaseModalDescription extends LightningElement {
-    @track isOpen = false;
+    codeSnippet = `<pre><code>&lt;c-base-modal&gt;
+    &lt;span slot="headerText"&gt;Lorem ipsum dolor&lt;/span&gt;
+    &lt;span slot="body"&gt;Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+        anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+        laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto
+        beatae vitae dicta sunt explicabo.
+    &lt;/span&gt;
+&lt;/c-base-modal&gt;</code></pre>`;
+    
     apiAttributes = [
         {
             id: '1',
-            apiName: 'variant',
-            defaultValue: 'A',
-            description: 'Sets the card base variant'
-        },
-        {
-            id: '2',
-            apiName: 'is-separator-visible',
-            defaultValue: 'false',
-            description: 'Sets a separator line between header and body'
-        },
-        {
-            id: '3',
-            apiName: 'is-footer-centered',
-            defaultValue: 'false',
-            description: 'Makes the footer centered'
+            apiName: 'open()',
+            defaultValue: '-',
+            description: 'Method for opening the modal'
         }
     ];
-    handleClick(){
-        this.isOpen = true;
-        console.log(this.isOpen);
-    }
-    handleClickClose(){
-        this.isOpen = false;
-        console.log(this.isOpen);
-    }
 
     handleOpenModal() { 
         const modal = this.template.querySelector("c-base-modal");
